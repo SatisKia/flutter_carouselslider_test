f718c0beb4640ba597482d24f56dfd7e7b8f5988
SatisKia <satis@msd.biglobe.ne.jp>
2022-02-27 23:11:39 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+import 'widget.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -27,17 +29,29 @@ class _MyHomePageState extends State {
   double contentWidth  = 0.0;
   double contentHeight = 0.0;
 
-  List<String> hogeSliderImageList = [
-    'assets/test1.jpg',
-    'assets/test2.jpg',
-    'assets/test3.jpg',
-  ];
   int hogeSliderPage = 0;
   Duration hogeAnimationDuration = Duration(milliseconds: 800);
   Curve hogeCurve = Curves.fastOutSlowIn;
   CarouselController hogeCarouselController = CarouselController();
-  void onTapCarouselSlider(int page){
+  void onTapHogeSlider(int page){
     debugPrint('${page + 1}ページ目がタップされた');
+  }
+  void onTapHogeController(int page){
+    if( hogeSliderPage != page ) {
+      setState(() {
+        hogeSliderPage = page;
+      });
+      hogeCarouselController.animateToPage(
+        hogeSliderPage,
+        duration: hogeAnimationDuration,
+        curve: hogeCurve,
+      );
+    }
+  }
+  void onPageChangedHogeSlider(int page){
+    setState(() {
+      hogeSliderPage = page;
+    });
   }
 
   @override
@@ -45,67 +59,55 @@ class _MyHomePageState extends State {
     contentWidth  = MediaQuery.of( context ).size.width;
     contentHeight = MediaQuery.of( context ).size.height - MediaQuery.of( context ).padding.top - MediaQuery.of( context ).padding.bottom;
 
+    List<Widget> hogeSliderPages = [
+      InkWell(
+          onTap: () => onTapHogeSlider(0),
+          child: Image.asset('assets/test1.jpg', fit: BoxFit.contain)
+      ),
+      InkWell(
+          onTap: () => onTapHogeSlider(1),
+          child: Image.asset('assets/test2.jpg', fit: BoxFit.contain)
+      ),
+      InkWell(
+          onTap: () => onTapHogeSlider(2),
+          child: Image.asset('assets/test3.jpg', fit: BoxFit.contain)
+      ),
+    ];
+
     double hogeViewportFraction = 0.8;
     double hogeSliderHeight = 214 * (contentWidth / 320) * hogeViewportFraction;
 
-    CarouselSlider hogeSlider = CarouselSlider(
-      items: hogeSliderImageList.map((item) {
-        int page = hogeSliderImageList.indexOf(item);
-        return InkWell(
-            onTap: () {
-              if( page == hogeSliderPage ) {
-                onTapCarouselSlider(page);
-              }
-            },
-            child: Image.asset(item, fit: BoxFit.contain)
-        );
-      }).toList(),
-      options: CarouselOptions(
-          aspectRatio: contentWidth / hogeSliderHeight,
-          viewportFraction: hogeViewportFraction,
-          initialPage: hogeSliderPage,
-          enableInfiniteScroll: true,
-          autoPlay: false,
-          autoPlayInterval: Duration(seconds: 4),
-          autoPlayAnimationDuration: hogeAnimationDuration,
-          autoPlayCurve: hogeCurve,
-          onPageChanged: (page, reason) {
-            if( reason != CarouselPageChangedReason.controller ) {
-              setState(() {
-                hogeSliderPage = page;
-              });
-            }
-          }
-      ),
+    CarouselSlider hogeSlider = MyCarouselSlider(
+      items: hogeSliderPages,
+      width: contentWidth,
+      height: hogeSliderHeight,
+      viewportFraction: hogeViewportFraction,
+      initialPage: hogeSliderPage,
+      enableInfiniteScroll: true,
+      autoPlay: false,
+      autoPlayInterval: Duration(seconds: 4),
+      autoPlayAnimationDuration: hogeAnimationDuration,
+      autoPlayCurve: hogeCurve,
+      onPageChanged: onPageChangedHogeSlider,
       carouselController: hogeCarouselController,
     );
 
     Row hogeController = Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: hogeSliderImageList.map((item) {
-        int page = hogeSliderImageList.indexOf(item);
-        return InkWell(
-            onTap: (){
-              setState(() {
-                hogeSliderPage = page;
-              });
-              hogeCarouselController.animateToPage(
-                hogeSliderPage,
-                duration: hogeAnimationDuration,
-                curve: hogeCurve,
-              );
-            },
-            child: Container(
-              width: 10,
-              height: 10,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: (page == hogeSliderPage) ? Color.fromARGB(255, 0, 0, 0) : Color.fromARGB(255, 192, 192, 192),
-              ),
-            )
-        );
-      }).toList(),
+      children: [
+        MyCarouselController(
+            onTap: () => onTapHogeController(0),
+            color: (hogeSliderPage == 0) ? Color.fromARGB(255, 0, 0, 0) : Color.fromARGB(255, 192, 192, 192)
+        ),
+        MyCarouselController(
+            onTap: () => onTapHogeController(1),
+            color: (hogeSliderPage == 1) ? Color.fromARGB(255, 0, 0, 0) : Color.fromARGB(255, 192, 192, 192)
+        ),
+        MyCarouselController(
+            onTap: () => onTapHogeController(2),
+            color: (hogeSliderPage == 2) ? Color.fromARGB(255, 0, 0, 0) : Color.fromARGB(255, 192, 192, 192)
+        ),
+      ],
     );
 
     return Scaffold(
---
+++ lib/widget.dart
@@ -0,0 +1,53 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class MyCarouselSlider extends CarouselSlider {
+  MyCarouselSlider({
+    required List<Widget> items,
+    required double width,
+    required double height,
+    double viewportFraction = 0.8,
+    int initialPage = 0,
+    bool enableInfiniteScroll = true,
+    bool autoPlay = false,
+    Duration autoPlayInterval = const Duration(seconds: 4),
+    Duration autoPlayAnimationDuration = const Duration(milliseconds: 800),
+    Curve autoPlayCurve = Curves.fastOutSlowIn,
+    required void Function(int) onPageChanged,
+    CarouselController? carouselController,
+    Key? key
+  }) : super(key: key,
+    items: items,
+    options: CarouselOptions(
+        aspectRatio: width / height,
+        viewportFraction: viewportFraction,
+        initialPage: initialPage,
+        enableInfiniteScroll: enableInfiniteScroll,
+        autoPlay: autoPlay,
+        autoPlayInterval: autoPlayInterval,
+        autoPlayAnimationDuration: autoPlayAnimationDuration,
+        autoPlayCurve: autoPlayCurve,
+        onPageChanged: (page, reason) {
+          if( reason != CarouselPageChangedReason.controller ) {
+            onPageChanged(page);
+          }
+        }
+    ),
+    carouselController: carouselController,
+  );
+}
+
+class MyCarouselController extends InkWell {
+  MyCarouselController({required Color color, required void Function() onTap, Key? key}) : super(key: key,
+    onTap: onTap,
+    child: Container(
+      width: 10,
+      height: 10,
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+      ),
+    ),
+  );
+}
